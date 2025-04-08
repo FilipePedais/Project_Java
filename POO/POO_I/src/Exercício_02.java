d49eadd91fd2b86deee4366ed43ddb8e0ab900dc
Filipe Pedais <filipepedais026@gmail.com>
2025-04-08 19:50:53 -0300

    popk

---
+++ POO/POO_I/src/Exercício_02.java
@@ -0,0 +1,18 @@
+public class Exercício_02
+{
+    public static void main (String [] args){
+        int SomaImprares = 0;
+        int MultiplicaPares = 1;
+        
+        for (int i = 1; i <= 20; i++){
+            if (i % 2 != 0){
+                SomaImprares += i;
+            } else {
+                MultiplicaPares *= i;
+            }
+        }
+        
+        System.out.println("Soma dos números Ímpares de 0 a 20: "+ SomaImprares);
+        System.out.println("Multiplicação dos números Pares de 0 a 20: "+ MultiplicaPares);
+    }
+}
---
+++ POO/POO_I/src/Exercício_03.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercício_03
+{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite a quantidade de horas trabalhadas: ");
+        double horastrabalhadas = scanner.nextDouble();
+
+        double valorHora = 30.0;
+
+        double salario = horastrabalhadas * valorHora;
+
+        System.out.println("O salário a receber é: R$"+ salario);
+
+        scanner.close();
+    }
+}
